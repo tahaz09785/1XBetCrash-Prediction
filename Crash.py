4975620c791cdcc30352d9e3f5237a15f9971503
tahaz09785 <taha.anazom.99@gmail.com>
2025-08-30 13:10:55 +0200

    Update Crash.py

--- Crash.py
+++ Crash.py
@@ -38,7 +38,7 @@ nn_reg = MLPRegressor(hidden_layer_sizes=(100,), max_iter=1000, random_state=123
 nn_reg.fit(train_X, train_y)
 
 # Create a Telegram bot object
-bot = telebot.TeleBot('YOUR_TOKEN')
+bot = telebot.TeleBot("8443726413:AAFm-P9zRlWIKMp6ILN58mAiqe13JRpytS0")
 
 # Define the handler function for the '/predict' command
 @bot.message_handler(commands=['predict'])
